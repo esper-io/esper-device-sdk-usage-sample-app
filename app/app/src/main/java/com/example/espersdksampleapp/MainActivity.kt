5afc9a54f1f5619469a4638c8f27a2c24a3451e4
Ayush Singhal <ayush.singhal.as30@gmail.com>
2021-06-29 12:40:05 +0530

    Implemented logic to check if esper agent is installed or not

--- app/app/src/main/java/com/example/espersdksampleapp/MainActivity.kt
+++ app/app/src/main/java/com/example/espersdksampleapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.espersdksampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.espersdksampleapp.databinding.ActivityMainBinding
 import io.esper.devicesdk.EsperDeviceSDK
 import io.esper.devicesdk.utils.EsperSDKVersions
@@ -16,8 +17,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView()
 
+        /*
+            NOTE: For Esper SDK to be functional,
+                  Esper Agent should be installed in the device.
+         */
+        if (!isEsperAgentInstalled()) {
+            Log.e(TAG, "onCreate: Error:: Esper Agent Not Found")
+            return
+        }
+
         // Get the instance of the Esper SDK
         sdk = EsperDeviceSDK.getInstance(applicationContext)
+    }
+
+    /**
+     * Method to check if Esper Agent is installed in the device or not.
+     *
+     * @return true if Esper Agent is installed else false
+     */
+    private fun isEsperAgentInstalled(): Boolean {
+        val esperDeviceSDKApiLevel = sdk.apiLevel
+
+        /*
+            If Esper Agent is not installed,
+            then getApiLevel() method returns EsperSDKVersions.INVALID_VERSION
+         */
+        if (esperDeviceSDKApiLevel == EsperSDKVersions.INVALID_VERSION) {
+            return false
+        }
+
+        return true
     }
 
     private fun setContentView() {
