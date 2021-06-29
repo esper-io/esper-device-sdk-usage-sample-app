0d3faad7aa74f34d8c32f7abe39781eb4ffb8fb3
Ayush Singhal <ayush.singhal.as30@gmail.com>
2021-06-29 12:27:39 +0530

    Implemented logic to get the instance of esper sdk

--- app/app/src/main/java/com/example/espersdksampleapp/MainActivity.kt
+++ app/app/src/main/java/com/example/espersdksampleapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.espersdksampleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.espersdksampleapp.databinding.ActivityMainBinding
+import io.esper.devicesdk.EsperDeviceSDK
+import io.esper.devicesdk.utils.EsperSDKVersions
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var sdk: EsperDeviceSDK
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView()
+
+        // Get the instance of the Esper SDK
+        sdk = EsperDeviceSDK.getInstance(applicationContext)
     }
 
     private fun setContentView() {
