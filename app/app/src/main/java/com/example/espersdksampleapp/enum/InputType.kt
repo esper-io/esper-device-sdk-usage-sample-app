1f789fed6149b31128596fd8231f4ef02105daba
Ayush Singhal <ayush.singhal.as30@gmail.com>
2021-07-05 15:59:58 +0530

    Added more fields

--- app/app/src/main/java/com/example/espersdksampleapp/enum/InputType.kt
+++ app/app/src/main/java/com/example/espersdksampleapp/enum/InputType.kt
@@ -1,22 +1,67 @@
 package com.example.espersdksampleapp.enum
 
+import android.view.View
+import android.view.View.OnClickListener
+
 sealed class InputType(
     val primaryHint: String? = null,
+    val primaryText: String? = null,
+
     val secondaryHint: String? = null,
+    val secondaryText: String? = null,
+
     val arrayResourceId: Int? = null,
+
     val switchText: String? = null,
-    val buttonText: String? = null
+
+    val buttonText: String? = null,
+    val buttonClickListener: OnClickListener? = null
 )
 
-class OneTextField(hint: String, buttonText: String? = null) :
-    InputType(primaryHint = hint, buttonText = buttonText)
+class OneTextField(
+    hint: String,
+    text: String? = null,
+    buttonText: String? = null,
+    buttonClickListener: OnClickListener? = null
+) :
+    InputType(
+        primaryHint = hint,
+        primaryText = text,
+        buttonText = buttonText,
+        buttonClickListener = buttonClickListener
+    )
 
-class TwoTextField(primaryHint: String, secondaryHint2: String) :
-    InputType(primaryHint = primaryHint, secondaryHint = secondaryHint2)
+class TwoTextField(
+    primaryHint: String,
+    primaryText: String? = null,
+    secondaryHint2: String,
+    secondaryText: String? = null,
+    buttonText: String? = null,
+    buttonClickListener: OnClickListener? = null
+) :
+    InputType(
+        primaryHint = primaryHint,
+        primaryText = primaryText,
+        secondaryHint = secondaryHint2,
+        secondaryText = secondaryText,
+        buttonText = buttonText,
+        buttonClickListener = buttonClickListener
+    )
 
-class Spinner(arrayResourceId: Int? = null, buttonText: String? = null) :
-    InputType(arrayResourceId = arrayResourceId, buttonText = buttonText)
+class Spinner(
+    arrayResourceId: Int? = null,
+    buttonText: String? = null,
+    buttonClickListener: OnClickListener? = null
+) :
+    InputType(
+        arrayResourceId = arrayResourceId,
+        buttonText = buttonText,
+        buttonClickListener = buttonClickListener
+    )
 
 class Switch(switchText: String? = null) : InputType(switchText = switchText)
 
-class OneTextFieldOneSpinner(buttonText: String? = null) : InputType(buttonText = buttonText)
+class OneTextFieldOneSpinner(
+    buttonText: String? = null,
+    buttonClickListener: OnClickListener? = null
+) : InputType(buttonText = buttonText, buttonClickListener = buttonClickListener)
